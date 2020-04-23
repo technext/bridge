71ed49c49f87b4df5e899975bdb5df8a6910e19f
kanijfatemalaizu <kanij.fatema.laizu@gmail.com>
2020-04-23 16:17:15 +0600

    first message

---
+++ assets/js/main.js
@@ -0,0 +1,15 @@
+var typed = new Typed('.element.typed-cursor',{
+	strings: ['developers.', 'designers.' , 'founders.'],
+	typeSpeed: 50,
+	backSpeed:50,
+	// showCursor: true,
+  	// cursorChar: '|',
+  	// autoInsertCss: true,
+	loop: true
+	});
+	
+	//slider
+	
+
+
+	
