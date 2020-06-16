68eb9138abd588da3693174d3119465e33f17915
kanijfatemalaizu <kanij.fatema.laizu@gmail.com>
2020-06-16 09:28:37 +0600

    message

---
+++ js/theme/typed.js
@@ -0,0 +1,20 @@
+/* eslint-disable */
+var typed = new Typed('.element.typed-cursor',{
+	strings: ['developers.', ' designers.' , ' founders.'],
+	typeSpeed: 50,
+	backSpeed:50,
+	// showCursor: true,
+  	// cursorChar: '|',
+  	// autoInsertCss: true,
+	loop: true
+	});
+// new window.Typed('.element.typed-cursor',{
+//     strings:['developers.', 'designers.' , 'founders.'],
+// 	typeSpeed: 50,
+// 	backSpeed:50,
+// 	// showCursor: true,
+//   	// cursorChar: '|',
+//   	// autoInsertCss: true,
+// 	loop: true
+// });
+	
